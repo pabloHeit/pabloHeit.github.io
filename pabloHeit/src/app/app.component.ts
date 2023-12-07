7175833fc980b37ef9eacfffdf0da2833e0b8287
pabloHeit <pabloaheit@yahoo.com.ar>
2023-12-07 17:38:42 -0300

    Bootstrap and styles

--- pabloHeit/src/app/app.component.ts
+++ pabloHeit/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pabloheit';
+
+  projects:any/* : [{img:string, link:string}] */ = [
+    {
+      name:"Lunfardo",
+      description:"Mazo de anki para aprender lunfardo",
+      img:"/assets/Img/Anki.png",
+      link:"/assets/Link/Lunfardo.apkg"
+    }/* ,
+    {
+      name:"Lunfardo",
+      description:"Lorem impum loren sadanem ajsdiasd its a uhh a",
+      img:"/assets/Img/Anki.png",
+      link:"/assets/Link/Lunfardo.apkg"
+    } */
+  ];
+
+  projectQuantity:Number = this.projects.length;
+
 }
